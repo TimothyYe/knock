b7622bf3588995e09fd6ff038775496b44391c9c
Timothy <yexiaozhou2003@gmail.com>
2024-03-01 01:54:59 +0800

    update test cases

--- src/sequence/port_sequence.rs
+++ src/sequence/port_sequence.rs
@@ -128,6 +128,7 @@ mod tests {
         detector.add_sequence("127.0.0.1".to_owned(), 3);
         detector.add_sequence("127.0.0.1".to_owned(), 5);
         detector.add_sequence("127.0.0.1".to_owned(), 6);
+        assert_eq!(detector.match_sequence("127.0.0.1"), false);
         assert_eq!(detector.client_sequences.get("127.0.0.1").unwrap().len(), 0);
     }
 }
